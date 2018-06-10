3428566507e78c01b262d891ebc5d578bc0950d3
MK3880 <34534769+MK3880@users.noreply.github.com>
2018-06-10 00:39:20 -0500

    whatever

--- controller/controller.js
+++ controller/controller.js
@@ -1,5 +1,5 @@
-//using this as a starting point because I still have no fucking clue how most of this works and my tutor bailed on me so ¯\_(ツ)_/¯
-
+//using this as a starting point because I still have no clue how most of this works and my tutor bailed on me so ¯\_(ツ)_/¯
+//PS: this isn't a straight copy from the activity, there are changes below that I hope were moving it in the right direction.
 var cheerio = require("cheerio");
 var request = require("request");
 
@@ -16,26 +16,57 @@ request("https://www.nhl.com/", function(error, response, html) {
   var $ = cheerio.load(html);
 
   // Empty array to save our scraped data
-  var results = [];
+  var titlesArray = [];
 
   // With cheerio, find each h4-tag with the class "headline-link" and loop through the results
   $("h4.headline-link").each(function(i, element) {
 
+    var result = {};
+
     // Save the text of the h4-tag as "title"
-    var title = $(element).text();
+    result.title = $(element).text();
 
     // Find the h4 tag's parent a-tag, and save it's href value as "link"
-    var link = $(element).parent().attr("href");
+    result.link = $(element).parent().attr("href");
 
-    // Make an object with data we scraped for this h4 and push it to the results array
-    results.push({
-      title: title,
-      link: link
+    if(result.title !== "" && result.link !== "") {
+      if(titlesArray.indexOf(result.title) == -1) {
+        titlesArray.push(result.title);
+
+        //add article if not there
+        Article.count({ title: result.title}, function (err,test){ 
+        if (test == 0){
+          var entry = new Article (result);
+          //save to mongodb
+          entry.save(function(err, doc) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(doc);
+            }
+          });
+        }
+      });
+    }
+
+    else{
+      console.log('Article already exists.')
+    }
+      }
+      else {
+        console.log('not save to DB, missing data')
+      }
     });
-    // I have no fucking clue how to get this info into the mongodb, even after referencing several other people's work on github.
-   
+    res.redirect('/');
   });
 
+    // Make an object with data we scraped for this h4 and push it to the results array
+  // results.push({
+    //  title: title,
+      //link: link
+    //});
+   
+ // });
+
   // After looping through each h4.headline-link, log the results
-  console.log(results);
-});
+  //console.log(results)
